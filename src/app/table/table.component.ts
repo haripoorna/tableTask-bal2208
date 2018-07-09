763526acefd3b54d67428a8c37a5e0597d13e62e
naveenreddysg <naveenreddy.sg@gmail.com>
2018-07-10 03:24:37 +0530

    topper check

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -11,6 +11,7 @@ export class TableComponent implements OnInit {
   constructor(private tableService: TableService) { }
   userData : any;
   totalMarks: Number = 0;
+  topper :any;
   ngOnInit() {
     this.userSearch();
   }
@@ -18,17 +19,33 @@ export class TableComponent implements OnInit {
   userSearch(){
     this.tableService.userSearch().subscribe(data=>{
       this.userData  = data;
+      this.calculateTotalMarks();
       this.checkStatus();
-      let i = 0;
-      while(i<=this.userData.length-1){
-            for (var key in this.userData[i].marks) {
-              this.totalMarks += this.totalMarks + this.userData[i].marks[key]
-                console.log(this.totalMarks)
-          }
-        i++
-      };
+      this.setTopper();
     })
-  }
+  };
+ 
+  calculateTotalMarks(){
+    let temp:any = [];
+    temp = this.userData;
+    let i = 0;
+    console.log(temp)
+    while(i<=temp.length-1){
+        //   for (var key in this.userData[i].marks) {
+        //     this.totalMarks += this.totalMarks + this.userData[i].marks[key]
+        //       console.log(this.totalMarks)
+        // }
+        if(temp[i].name == this.userData[i].name){
+          let totalMarks:any = 0
+          for (var key in temp[i].marks) {
+             totalMarks = totalMarks + temp[i].marks[key];
+          }
+          this.userData[i].totalMarks = totalMarks;
+        }
+      i++
+    };
+  };
+  
   checkStatus(){
     for(let i=0; i<=this.userData.length-1; i++){
       let marks = this.userData[i].marks;
@@ -37,12 +54,20 @@ export class TableComponent implements OnInit {
                 if(this.userData[i].marks[key]<20){
                   this.userData[i].status = "Failed";
                   break;
-                }else{
+                }
+                else{
                   this.userData[i].status = "Pass";
                 }
             }
         }
     };
   }
-
+  setTopper(){  
+    this.topper = this.userData.reduce((min, p) => p.totalMarks > min ? p.totalMarks : min, this.userData[0].totalMarks);
+    this.userData.forEach(element => {
+      if(element.totalMarks == this.topper){
+        element.status = "Topper";
+      }
+    });
+  }
 }
